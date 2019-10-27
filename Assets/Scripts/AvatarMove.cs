ab53e636f24f2edb10f4be7dbad5d3394b6531e8
Bc. Stanislav Husár <stanislavhusar@icloud.com>
2019-10-27 14:30:31 +0100

    VR Grabbable

---
+++ Assets/Scripts/AvatarMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarMove : MonoBehaviour
+{
+    public float RotationSpeed;
+    public float MoveSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * MoveSpeed;
+        float deltaZ = Input.GetAxis("Vertical") * MoveSpeed;
+        float deltaR = Input.GetAxis("Rotational") * RotationSpeed;
+
+        transform.Rotate(0, deltaR, 0, Space.Self);
+        transform.Translate(deltaX, 0, deltaZ, Space.Self);
+    }
+}
---
+++ Assets/Scripts/SpawnGrabbables.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGrabbables : MonoBehaviour
+{
+    public Bounds SpawnSpace;
+    public int NumGrabbables;
+    public GameObject GrabbablePrefab;
+
+    void Start()
+    {
+        for(int i = 0;i < NumGrabbables;i++)
+        {
+            float x = Random.Range(SpawnSpace.min.x, SpawnSpace.max.x);
+            float y = Random.Range(SpawnSpace.min.y, SpawnSpace.max.y);
+            float z = Random.Range(SpawnSpace.min.z, SpawnSpace.max.z);
+
+            Instantiate(GrabbablePrefab, new Vector3(x, y, z), Quaternion.identity);
+        }
+    }
+}
