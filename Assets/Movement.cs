fd8d85872765f8df7e0006b6ce579015730a9a02
olivertomko8 <56840494+olivertomko8@users.noreply.github.com>
2019-10-29 11:15:14 +0100

    Add files via upload

---
+++ Assets/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float speed;
+    private void Start()
+    {
+        speed = 5;
+    }
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    }
+}
