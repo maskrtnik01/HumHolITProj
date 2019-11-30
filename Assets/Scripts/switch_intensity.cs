a69943fb123e865bf4436f0f944416d68b4ac8a0
lubomirrusnak <56437631+lubomirrusnak@users.noreply.github.com>
2019-11-30 02:24:49 +0100

    zmazanie nepotrebných funkcii

--- Assets/Scripts/switch_intensity.cs
+++ Assets/Scripts/switch_intensity.cs
@@ -4,27 +4,11 @@ using UnityEngine;
 
 public class switch_intensity : MonoBehaviour
 {
-    public Light myLight;
-
-    public bool changeRange = false;
-    public float rangeSpeed = 1.0f;
-    public float maxRange = 10.0f;
-
-    public bool changeIntensity = false;
-    public float intensitySpeed = 1.0f;
-    public float maxIntensity = 10.0f;
-
-    public bool changeColor = false;
-    public float colorSpeed = 1.0f;
-    public Color startColor;
-    public Color endColor;
-
-    float startTime;
+    
 
     void Start()
     {
-        myLight = GetComponent<Light>();
-        startTime = Time.time;
+     
     }
 
     // Update is called once per frame
@@ -35,19 +19,7 @@ public class switch_intensity : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
             this.GetComponent<Light>().enabled = false;
 
-        /*if (changeRange)
-        {
-            myLight.range = Mathf.PingPong(Time.time * rangeSpeed, maxRange);
-        }
-        if (changeIntensity)
-        {
-            myLight.range = Mathf.PingPong(Time.time * intensitySpeed, maxIntensity);
-        }
-        if (changeColor )
-        {
-            float t = (Mathf.Sin(Time.time - startTime * colorSpeed));
-            myLight.color = Color.Lerp(startColor, endColor, t);
-        }*/
+        
 
     }
 }
