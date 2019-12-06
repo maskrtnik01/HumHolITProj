ddc407ee24a5c7d6e821ee48dfae2f4119d8b9cc
Stanislav Husár <stanislavhusar@icloud.com>
2019-12-06 21:20:35 +0100

    Manipulate devices with avatar

---
+++ Assets/Scripts/SampleManipulatedDevice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleManipulatedDevice : MonoBehaviour
+{
+    public void OnProximityEnter()
+    {
+        Debug.Log("OnProximityEnter");
+    }
+    public void OnProximityExit()
+    {
+        Debug.Log("OnProximityExit");
+    }
+    public void OnPrimaryFunction()
+    {
+        Debug.Log("OnPrimaryFunction");
+    }
+}
