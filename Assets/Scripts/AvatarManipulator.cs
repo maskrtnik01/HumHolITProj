9532ff11b1e80111af0ebc03b77db05cdca8db21
olivertomko8 <56840494+olivertomko8@users.noreply.github.com>
2019-12-12 23:25:14 +0100

    oprava_objektov

---
+++ Assets/Scripts/AvatarManipulator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarManipulator : MonoBehaviour
+{
+    List<GameObject> TriggersList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TriggersList = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+            foreach (var trig in TriggersList)
+                trig.BroadcastMessage("OnPrimaryFunction", SendMessageOptions.DontRequireReceiver);    
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggersList.Add(other.gameObject);
+        other.gameObject.BroadcastMessage("OnProximityEnter",SendMessageOptions.DontRequireReceiver);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(TriggersList.Contains(other.gameObject))
+        {
+            TriggersList.Remove(other.gameObject);
+            other.gameObject.BroadcastMessage("OnProximityExit", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
---
+++ Assets/Scripts/LightManipulator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManipulator : MonoBehaviour
+{
+    public Light light;
+    public void OnPrimaryFunction()
+    {
+        light.enabled = !light.enabled;
+    }
+}
