a40e580447e0315bbbfe60f3de42fa262df215fc
Stanislav Husár <stanislavhusar@icloud.com>
2019-12-14 15:31:57 +0100

    Christmas switch

---
+++ Assets/Scripts/ThemeManipulator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemeManipulator : MonoBehaviour
+{
+    public GameObject Target;
+    public void OnPrimaryFunction()
+    {
+        Target.SetActive(!Target.activeSelf);
+    }
+}
