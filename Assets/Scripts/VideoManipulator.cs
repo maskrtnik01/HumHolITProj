d429d304f8c4aaa937c159ba178af0622c096cb8
Stanislav Husár <stanislavhusar@icloud.com>
2019-12-16 09:06:35 +0100

    Video on TV

---
+++ Assets/Scripts/VideoManipulator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoManipulator : MonoBehaviour
+{
+    private VideoPlayer plr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plr = GetComponent<VideoPlayer>();
+    }
+    void OnPrimaryFunction()
+    {
+        if (plr.isPlaying)
+            plr.Stop();
+        else
+            plr.Play();
+    }
+}
